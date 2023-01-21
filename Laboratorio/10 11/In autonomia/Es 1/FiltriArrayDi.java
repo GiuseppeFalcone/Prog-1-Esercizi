752fd33c2d7c991fba1690b0bea713cd04cd46b5
GiuseppeFalcone <giuseppe001falcone@gmail.com>
2023-01-21 19:36:40 +0100

    Continuazione del laboratorio del 10/11

--- Laboratorio/10 11/In autonomia/Es 1/FiltriArrayDi.java
+++ Laboratorio/10 11/In autonomia/Es 1/FiltriArrayDi.java
@@ -136,10 +136,189 @@ public class FiltriArrayDi{
     ///////////////////////////////////////////////////////////////////////
 
     public static int[] soloElDispariV2(int[] a){
+        int[] b = null;
         if (a != null){
-            
+            b = new int[]{};
+            b = soloElDispariV2(a, 0, a.length, b);
+        }
+        return b;
+    }
+
+    private static int[] soloElDispariV2(int[] a, int l, int r, int[] b){
+        if (l+1 == r){
+            if (a[l] % 2 != 0){
+                int[] tmp = new int[b.length + 1];
+                int i = 0;
+                while (i < b.length){
+                    tmp[i] = b[i];
+                    i = i + 1;
+                }
+                tmp[b.length] = a[l];
+                b = tmp;
+            }
+            return b;
+        } else {
+            int m = ((l+r)/2);
+            b = soloElDispariV2(a, l, m, b);
+            b = soloElDispariV2(a, m, r, b);
+            return b;
         }
     }
 
+/*=====================================================================================*/
 
+    public static int[] elPosPariV1(int[] a){
+        int[] b = null;
+        if (a != null){
+            b = new int[elPosPariV1(a, 0, a.length)];
+            elPosPariV1(a, 0, a.length, b);
+        }
+        return b;
+    }
+
+    private static int elPosPariV1(int[] a, int l, int r){
+        if (l+1 == r){
+            if (l % 2 == 0){
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            int m = ((l+r) / 2);
+            return (elPosPariV1(a, l, m) + elPosPariV1(a, m, r));
+        }
+    }
+
+    private static int[] elPosPariV1(int[] a, int l, int r, int[] b){
+        if (l+1 == r){
+            if (l % 2 == 0){
+                int i = 0;
+                boolean controllo = true;
+                while (controllo && i < b.length){
+                    if (b[i] == 0){
+                        b[i] = a[l];
+                        controllo = !controllo;
+                    }
+                    i = i + 1;
+                }
+            }
+            return b;
+        } else {
+            int m = ((l+r) / 2);
+            b = elPosPariV1(a, l, m, b);
+            b = elPosPariV1(a, m, r, b);
+            return b;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+
+    public static int[] elPosPariV2(int[] a){
+        if (a != null){
+            int[] b = new int[]{};
+            b = elPosPariV2(a, 0, a.length, b);
+            return b;
+        } else {
+            return null;
+        }
+    }
+
+    private static int[] elPosPariV2(int[] a, int l, int r, int[] b){
+        if (l+1 == r){
+            if (l % 2 == 0){
+                int[] tmp = new int[b.length + 1];
+                int i = 0;
+                while (i < b.length){
+                    tmp[i] = b[i];
+                    i = i + 1;
+                }
+                tmp[b.length] = a[l];
+                b = tmp;
+            }
+            return b;
+
+        } else {
+            int m = ((l+r) / 2);
+            b = elPosPariV2(a, l, m, b);
+            b = elPosPariV2(a, m, r, b);
+            return b;
+        }
+    }
+
+/*=====================================================================================*/
+
+    public static int[] elPosDispValParV1(int[] a){
+        int[] b = null;
+        if (a != null){
+            b = new int[elPosDispValParV1(a, 0, a.length)];
+            elPosDispValParV1(a, 0, a.length, b);
+        }
+        return b;
+    }
+    private static int elPosDispValParV1(int[] a, int l, int r){
+        if (l+1 == r){
+            if ((l % 2 != 0) && (a[l] % 2 == 0)){
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            int m = ((l+r)/2);
+            return (elPosDispValParV1(a, l, m) + elPosDispValParV1(a, m, r));
+        }
+    }
+
+    private static void elPosDispValParV1(int[] a, int l, int r, int[] b){
+        if (l+1 == r){
+            if ((l % 2 != 0) && (a[l] % 2 == 0)){
+                int i = 0;
+                boolean controllo = true;
+                
+                while (controllo && i < b.length){
+                    if (b[i] == 0){
+                        b[i] = a[l];
+                        controllo = !controllo;
+                    }
+                    i = i+1;
+                }
+            }
+        } else {
+            int m = ((l+r) / 2);
+            elPosDispValParV1(a, l, m, b);
+            elPosDispValParV1(a, m, r, b);
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////
+    
+    public static int[] elPosDispValParV2(int[] a){
+        int[] b = null;
+        if (a != null){
+            b = new int[]{};
+            b = elPosDispValParV2(a, 0, a.length, b);
+        }
+        return b;
+    }
+    
+    private static int[] elPosDispValParV2(int[] a, int l, int r, int[] b){
+        if (l+1 == r){
+            if ((l % 2 != 0) && (a[l] % 2 == 0)){
+                int[] tmp = new int[b.length + 1];
+                int i = 0;
+
+                while (i < b.length){
+                    tmp[i] = b[i];
+                    i = i + 1;
+                }
+                tmp[b.length] = a[l];
+                b = tmp;
+            }
+            return b;
+        } else {
+            int m = ((l+r)/2);
+            b = elPosDispValParV2(a, l, m, b);
+            b = elPosDispValParV2(a, m, r, b);
+            return b;
+        }
+    }
 }
--- Laboratorio/10 11/In autonomia/Es 1/FiltriArrayDiTest.java
+++ Laboratorio/10 11/In autonomia/Es 1/FiltriArrayDiTest.java
@@ -8,9 +8,22 @@ public class FiltriArrayDiTest{
         int[] b2 = FiltriArrayDi.limiteSuperioreVersDue(a, 4);
         System.out.println(Arrays.toString(b2));
         
-        int[] b3 = FiltriArrayDi.soloElDispari(a);
+        int[] b3 = FiltriArrayDi.soloElDispariV1(a);
         System.out.println(Arrays.toString(b3));
     
-    
+        int[] b4 = FiltriArrayDi.soloElDispariV2(a);
+        System.out.println(Arrays.toString(b4));
+
+        int[] b5 = FiltriArrayDi.elPosPariV1(a);
+        System.out.println(Arrays.toString(b5));
+        
+        int[] b6 = FiltriArrayDi.elPosPariV2(a);
+        System.out.println(Arrays.toString(b6));
+        
+        int[] b7 = FiltriArrayDi.elPosDispValParV1(a);
+        System.out.println(Arrays.toString(b7));
+        
+        int[] b8 = FiltriArrayDi.elPosDispValParV2(a);
+        System.out.println(Arrays.toString(b8));
     }
 }
